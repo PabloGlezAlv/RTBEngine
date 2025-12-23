057220a4250bc8d986fd2fed22f202cdf619a0cb
PabloGlezAlv <pablogonzalva2001@gmail.com>
2025-12-23 20:08:37 +0100

    UIImage

---
+++ RTBEngine/Engine/UI/Elements/UIImage.cpp
@@ -0,0 +1,52 @@
+#include "UIImage.h"
+#include <imgui.h>
+
+namespace RTBEngine {
+	namespace UI {
+
+		UIImage::UIImage()
+			: texture(nullptr)
+			, tintColor(1.0f, 1.0f, 1.0f, 1.0f)
+			, preserveAspect(true)
+		{
+		}
+
+		UIImage::~UIImage() {
+		}
+
+		void UIImage::SetTexture(Rendering::Texture* tex) {
+			texture = tex;
+		}
+
+		void UIImage::SetTint(const Math::Vector4& tint) {
+			tintColor = tint;
+		}
+
+		void UIImage::SetPreserveAspect(bool preserve) {
+			preserveAspect = preserve;
+		}
+
+		void UIImage::Render() {
+			if (!isVisible || !texture) return;
+
+			Math::Vector4 screenRect = rectTransform->GetScreenRect();
+			ImDrawList* drawList = ImGui::GetBackgroundDrawList();
+
+			ImVec2 min(screenRect.x, screenRect.y);
+			ImVec2 max(screenRect.x + screenRect.z, screenRect.y + screenRect.w);
+
+			ImVec2 uv0(0.0f, 0.0f);
+			ImVec2 uv1(1.0f, 1.0f);
+
+			ImU32 tint = IM_COL32(
+				static_cast<int>(tintColor.x * 255),
+				static_cast<int>(tintColor.y * 255),
+				static_cast<int>(tintColor.z * 255),
+				static_cast<int>(tintColor.w * 255)
+			);
+
+			drawList->AddImage((ImTextureID)(intptr_t)texture->GetID(), min, max, uv0, uv1, tint);
+		}
+
+	}
+}
